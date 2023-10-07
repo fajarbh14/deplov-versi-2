71d4647b0e377f8137ba9ac723d65c3a131bb8e5
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-10-08 00:13:53 +0700

    feat: recommendation explain

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def predict(x):
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
         file = request.files.get('file')
@@ -57,7 +57,6 @@ def index():
             tensor = transform_image(pillow_img)
             prediction = predict(tensor)
 
-            # Gantilah 'label_names' dengan nama label yang sesuai dengan indeks numerik.
             label_names = {0: 'Tidak ada DR (Tidak ada Retinopati Diabetes)',
                            1: 'DR Ringan (Mild)',
                            2: 'DR Sedang (Moderate)',
@@ -72,51 +71,55 @@ def index():
             recommendation = {}
 
             if label_name == 'Tidak ada DR (Tidak ada Retinopati Diabetes)':
-                message = ["Lakukan Kontrol Gula Darah Secara Berkala"]
-                general_recommendation = ["Cara Menurunkan Gula Darah dengan Alam",
-                                          "1 Berolahraga secara teratur.",
-                                          "2 Minum banyak air putih.",
-                                          "3 Konsumsi makanan berserat.",
-                                          "4 Konsumsi makanan kaya protein.",
-                                          "5 Konsumsi makanan kaya kromium"]
+                message = ["Lakukan Kontrol Kadar Gula Darah Secara Berkala"]
+                general_recommendation = ["Cara mengontrol kadar gula darah\n",
+                                          "1. Konsumsi makanan yang tepat: Menurut studi dalam jurnal Education and Health Promotion, melewatkan jadwal makan terlalu lama malah akan menyebabkan gula darah turun dan kemudian melonjak cepat.\n",
+                                          "2. Mengontrol porsi makan: Diabetesi dengan berat badan normal juga sebaiknya menjaga porsi makannya sehingga tidak berujung obesitas.\n",
+                                          "3. Aktif bergerak dan olahraga teratur: Olahraga dapat membantu sel-sel di otot Anda mengambil lebih banyak glukosa dan mengubahnya menjadi energi, sehingga mampu menurunkan gula darah. Hindari gaya hidup sedentari (bermalas-malasan) dan minim gerakan fisik atau membuang energi, seperti menonton TV, bermain game pada gawai, atau duduk terlalu lama di depan komputer.\n",
+                                          "4. Kelola stess dengan baik: Stres berlebihan juga dapat menyebabkan kadar gula darah meningkat akibat pelepasan kortisol alias hormon stres. Nah, agar stres tidak sampai membuat kadar gula darah melonjak, penting untuk Anda memahami cara mengendalikan stres dan mencoba berbagai hal yang dapat memperbaiki suasana hati, merilekskan tubuh, dan menenangkan pikiran.\n",
+                                          "5. Istirahat Cukup: Tidur yang cukup dapat menyeimbangkan hormon, menghindari stres, dan membuat Anda mendapatkan cukup energi untuk beraktivitas dan berolahraga pada esok harinya.Dengan demikian, kadar gula darah pun dapat terkendali dengan baik.\n",
+                                          "6. Rutin mengecek gula darah: Dengan terus memantau perubahan kadar gula darah, Anda akan lebih mudah untuk menentukan apakah harus melakukan penyesuaian pola makan atau konsumsi obat."
+                                          ]
                 recommendation = {
                     "message": message,
                     "general_recommendation": general_recommendation
                 }
             elif label_name == 'DR Ringan (Mild)':
-                message = ["Lakukan Kontrol Gula Darah Secara Berkala"]
-                general_recommendation = ["Cara Menurunkan Gula Darah dengan Alam",
-                                          "1 Berolahraga secara teratur.",
-                                          "2 Minum banyak air putih.",
-                                          "3 Konsumsi makanan berserat.",
-                                          "4 Konsumsi makanan kaya protein.",
-                                          "5 Konsumsi makanan kaya kromium"]
+                message = ["Lakukan Kontrol Kadar Gula Darah Secara Berkala"]
+                general_recommendation = ["Cara mengontrol kadar gula darah\n",
+                                          "1. Konsumsi makanan yang tepat: Menurut studi dalam jurnal Education and Health Promotion, melewatkan jadwal makan terlalu lama malah akan menyebabkan gula darah turun dan kemudian melonjak cepat.\n",
+                                          "2. Mengontrol porsi makan: Diabetesi dengan berat badan normal juga sebaiknya menjaga porsi makannya sehingga tidak berujung obesitas.\n",
+                                          "3. Aktif bergerak dan olahraga teratur: Olahraga dapat membantu sel-sel di otot Anda mengambil lebih banyak glukosa dan mengubahnya menjadi energi, sehingga mampu menurunkan gula darah. Hindari gaya hidup sedentari (bermalas-malasan) dan minim gerakan fisik atau membuang energi, seperti menonton TV, bermain game pada gawai, atau duduk terlalu lama di depan komputer.\n",
+                                          "4. Kelola stess dengan baik: Stres berlebihan juga dapat menyebabkan kadar gula darah meningkat akibat pelepasan kortisol alias hormon stres. Nah, agar stres tidak sampai membuat kadar gula darah melonjak, penting untuk Anda memahami cara mengendalikan stres dan mencoba berbagai hal yang dapat memperbaiki suasana hati, merilekskan tubuh, dan menenangkan pikiran.\n",
+                                          "5. Istirahat Cukup: Tidur yang cukup dapat menyeimbangkan hormon, menghindari stres, dan membuat Anda mendapatkan cukup energi untuk beraktivitas dan berolahraga pada esok harinya.Dengan demikian, kadar gula darah pun dapat terkendali dengan baik.\n",
+                                          "6. Rutin mengecek gula darah: Dengan terus memantau perubahan kadar gula darah, Anda akan lebih mudah untuk menentukan apakah harus melakukan penyesuaian pola makan atau konsumsi obat."
+                                          ]
                 recommendation = {
                     "message": message,
                     "general_recommendation": general_recommendation
                 }
             elif label_name == 'DR Sedang (Moderate)':
                 message = ["Lakukan konsultasi ke Dokter"]
-                general_recommendation = ["Hal-hal yang mungkin dilakukan setelah melakukan konsultasi bersama Dokter:",
-                                          "1 Injeksi mata: Dokter akan menyuntikan steroid pada mata untuk menghentikan peradangan dan mencegah pembentukan pembuluh darah baru. Suntikan anti-VEGF juga mungkin disarankan, yang dapat mengurangi pembengkakan di makula dan meningkatkan penglihatan.",
-                                          "2 Operasi Leser: Operasi laser yang disebut fotokoagulasi mengurangi pembengkakan di retina dan menghilangkan pembuluh darah abnormal.",
-                                          "3 Vitrektomi: Jika Anda menderita retinopati diabetik stadium lanjut, Anda mungkin memerlukan vitrektomi. Operasi mata ini mengatasi masalah pada retina dan vitreous, zat seperti jeli di tengah mata. Operasi tersebut dapat menghilangkan darah atau cairan, jaringan parut, dan sebagian gel vitreous sehingga sinar cahaya dapat terfokus dengan baik pada retina."]
+                general_recommendation = ["Hal-hal yang mungkin dilakukan setelah melakukan konsultasi bersama Dokter\n",
+                                          "1. Injeksi mata: Dokter akan menyuntikan steroid pada mata untuk menghentikan peradangan dan mencegah pembentukan pembuluh darah baru. Suntikan anti-VEGF juga mungkin disarankan, yang dapat mengurangi pembengkakan di makula dan meningkatkan penglihatan.\n",
+                                          "2. Operasi Leser: Operasi laser yang disebut fotokoagulasi mengurangi pembengkakan di retina dan menghilangkan pembuluh darah abnormal.\n",
+                                          "3. Vitrektomi: Jika Anda menderita retinopati diabetik stadium lanjut, Anda mungkin memerlukan vitrektomi. Operasi mata ini mengatasi masalah pada retina dan vitreous, zat seperti jeli di tengah mata. Operasi tersebut dapat menghilangkan darah atau cairan, jaringan parut, dan sebagian gel vitreous sehingga sinar cahaya dapat terfokus dengan baik pada retina.\n"]
                 recommendation = {"message": message,
                                   "general_recommendation": general_recommendation}
             elif label_name == 'DR Parah (Severe)':
                 message = ["Lakukan konsultasi ke Dokter"]
-                general_recommendation = ["Hal-hal yang mungkin dilakukan setelah melakukan konsultasi bersama Dokter:",
-                                          "1 Injeksi mata: Dokter akan menyuntikan steroid pada mata untuk menghentikan peradangan dan mencegah pembentukan pembuluh darah baru. Suntikan anti-VEGF juga mungkin disarankan, yang dapat mengurangi pembengkakan di makula dan meningkatkan penglihatan.",
-                                          "2 Operasi Leser: Operasi laser yang disebut fotokoagulasi mengurangi pembengkakan di retina dan menghilangkan pembuluh darah abnormal.",
-                                          "3 Vitrektomi: Jika Anda menderita retinopati diabetik stadium lanjut, Anda mungkin memerlukan vitrektomi. Operasi mata ini mengatasi masalah pada retina dan vitreous, zat seperti jeli di tengah mata. Operasi tersebut dapat menghilangkan darah atau cairan, jaringan parut, dan sebagian gel vitreous sehingga sinar cahaya dapat terfokus dengan baik pada retina."]
+                general_recommendation = ["Hal-hal yang mungkin dilakukan setelah melakukan konsultasi bersama Dokter\n",
+                                          "1. Injeksi mata: Dokter akan menyuntikan steroid pada mata untuk menghentikan peradangan dan mencegah pembentukan pembuluh darah baru. Suntikan anti-VEGF juga mungkin disarankan, yang dapat mengurangi pembengkakan di makula dan meningkatkan penglihatan.\n",
+                                          "2. Operasi Leser: Operasi laser yang disebut fotokoagulasi mengurangi pembengkakan di retina dan menghilangkan pembuluh darah abnormal.\n",
+                                          "3. Vitrektomi: Jika Anda menderita retinopati diabetik stadium lanjut, Anda mungkin memerlukan vitrektomi. Operasi mata ini mengatasi masalah pada retina dan vitreous, zat seperti jeli di tengah mata. Operasi tersebut dapat menghilangkan darah atau cairan, jaringan parut, dan sebagian gel vitreous sehingga sinar cahaya dapat terfokus dengan baik pada retina.\n"]
                 recommendation = {"message": message,
                                   "general_recommendation": general_recommendation}
             elif label_name == 'DR Proliferatif (Proliferative)':
                 message = ["Lakukan konsultasi ke Dokter"]
-                general_recommendation = ["Hal-hal yang mungkin dilakukan setelah melakukan konsultasi bersama Dokter:",
-                                          "1 Injeksi mata: Dokter akan menyuntikan steroid pada mata untuk menghentikan peradangan dan mencegah pembentukan pembuluh darah baru. Suntikan anti-VEGF juga mungkin disarankan, yang dapat mengurangi pembengkakan di makula dan meningkatkan penglihatan.",
-                                          "2 Operasi Leser: Operasi laser yang disebut fotokoagulasi mengurangi pembengkakan di retina dan menghilangkan pembuluh darah abnormal.",
-                                          "3 Vitrektomi: Jika Anda menderita retinopati diabetik stadium lanjut, Anda mungkin memerlukan vitrektomi. Operasi mata ini mengatasi masalah pada retina dan vitreous, zat seperti jeli di tengah mata. Operasi tersebut dapat menghilangkan darah atau cairan, jaringan parut, dan sebagian gel vitreous sehingga sinar cahaya dapat terfokus dengan baik pada retina."]
+                general_recommendation = ["Hal-hal yang mungkin dilakukan setelah melakukan konsultasi bersama Dokter\n",
+                                          "1. Injeksi mata: Dokter akan menyuntikan steroid pada mata untuk menghentikan peradangan dan mencegah pembentukan pembuluh darah baru. Suntikan anti-VEGF juga mungkin disarankan, yang dapat mengurangi pembengkakan di makula dan meningkatkan penglihatan.\n",
+                                          "2. Operasi Leser: Operasi laser yang disebut fotokoagulasi mengurangi pembengkakan di retina dan menghilangkan pembuluh darah abnormal.\n",
+                                          "3. Vitrektomi: Jika Anda menderita retinopati diabetik stadium lanjut, Anda mungkin memerlukan vitrektomi. Operasi mata ini mengatasi masalah pada retina dan vitreous, zat seperti jeli di tengah mata. Operasi tersebut dapat menghilangkan darah atau cairan, jaringan parut, dan sebagian gel vitreous sehingga sinar cahaya dapat terfokus dengan baik pada retina.\n"]
                 recommendation = {"message": message,
                                   "general_recommendation": general_recommendation}
 
